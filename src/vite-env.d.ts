9d772493c9e9ccc76ba06f9d486df357451f6ffb
格子 <fl9420@qq.com>
2025-09-07 16:13:42 +0800

    chore: 使用 vite biome

---
+++ src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+declare module "*.json" {
+	const value: any;
+	export default value;
+}
+
+declare module "*.vue" {
+	import type { DefineComponent } from "vue";
+	const component: DefineComponent<{}, {}, any>;
+	export default component;
+}
